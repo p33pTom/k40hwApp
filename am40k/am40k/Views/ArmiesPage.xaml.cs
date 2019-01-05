bd00f18da89ee38ad0600911ff1fcc07d15f802a
ptom <igupalo@protonmail.com>
2019-01-05 18:00:58 +0200

    #162999903

--- am40k/am40k/Views/ArmiesPage.xaml.cs
+++ am40k/am40k/Views/ArmiesPage.xaml.cs
@@ -26,10 +26,7 @@ namespace am40k
                 Text = "Add"
             };
 
-            Button AddButton2 = new Button
-            {
-                Text = "Add2"
-            };
+            
 
             ArmyPicker.SelectedIndexChanged += (sender, e) =>
             {
